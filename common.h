e1e433b3ee7198a216d1f277643f15c4ee4ad9c2
Filip <filip@KOMPUTER_FILIP>
2026-01-12 23:30:01 +0100

    Added client logic and client generator

--- common.h
+++ common.h
@@ -13,47 +13,50 @@
 #include <signal.h>
 #include <time.h>
 #include <string.h>
+#include <sys/wait.h>
 
 // --- KONFIGURACJA ---
 #define FTOK_PATH "."
 #define ID_PROJEKTU 10
 
 // Limity
+#define MAX_PRODUKTOW 10
+#define MIN_PRODUKTOW 3
 #define LICZBA_KAS_STACJONARNYCH 2
 #define LICZBA_KAS_SAMOOBSLUGOWYCH 6
 #define MAX_KLIENTOW_W_SKLEPIE 50 // wczytane N (domyslne 50)
-#define LIMIT_KLIENTOW_NA_KASE 5  // wczytane  K (domyslne 5)
+#define LIMIT_KLIENTOW_NA_KASE 5   // wczytane K (domyslne 5)
 
 // --- STRUKTURY DANYCH ---
 
 // Pamiec wspoldzielona - Stan Sklepu
 // Dostep choniony semaforem (SEM_STAN)!
 typedef struct {
-        int liczba_klientow_w_sklepie;
+	int liczba_klientow_w_sklepie;
 
-        // Liczniki kolejek (dla Kierownika)
-        int kolejka_samoobslugowa_len;
-        int kolejka_stacjonarna_len[LICZBA_KAS_STACJONARNYCH];
+	// Liczniki kolejek (dla Kierownika)
+	int kolejka_samoobslugowa_len;
+	int kolejka_stacjonarna_len[LICZBA_KAS_STACJONARNYCH];
 
-        // Statusy kas
-        // Stacjonarne: 0 = zamknieta 1 = otwarta
-        int kasy_stacjonarne_status[LICZBA_KAS_STACJONARNYCH];
+	// Statusy kas
+	// Stacjonarne: 0 = zamknieta 1 = otwarta
+	int kasy_stacjonarne_status[LICZBA_KAS_STACJONARNYCH];
 
-        // Samoobslugowe: 0 = wolna, pid_klienta = zajeta, -1 = zablokowana (wymaga pomocy)
-        int kasy_samoobslugowe_status[LICZBA_KAS_SAMOOBSLUGOWYCH];
+	// Samoobslugowe: 0 = wolna, pid_klienta = zajeta, -1 = zablokowana (wymaga pomocy)
+	int kasy_samoobslugowe_status[LICZBA_KAS_SAMOOBSLUGOWYCH];
 
-        // Flaga dla Kierownika, aby wiedzial kiedy zakonczyc symulacje
-        int koniec_symulacji;
+	// Flaga dla Kierownika, aby wiedzial kiedy zakonczyc symulacje
+	int koniec_symulacji;
 } StanSklepu;
 
 // Komunikat w kolejce komunikatow
-// Klienci wysylaja 'chce podejsc do kasy;
+// Klienci wysylaja 'chce podejsc do kasy'
 // Kasy odbieraja 'obsluguje cie'
 typedef struct {
-        long mtype;       // 1 = do samoobslugowej, 2 = do stacjonarnej 1, 3 = do stacjonarnej 2
-        pid_t id_klienta; // PID procesu klienta
-        int liczba_produktow;
-        int czy_alkohol;  // 1 = tak, 0 = nie
+	long mtype;       // 1 = do samoobslugowej, 2 = do stacjonarnej 1, 3 = do stacjonarnej 2
+	pid_t id_klienta; // PID procesu klienta
+	int liczba_produktow;
+	int czy_alkohol;  // 1 = tak, 0 = nie
 } Komunikat;
 
 // --- SEMAFORY ---
@@ -65,9 +68,9 @@ typedef struct {
 // --- MAKRA POMOCNICZE ---
 // obsluga bledow
 #define CHECK(x, msg) \
-        if ((x) == -1) { \
-        perror(msg); \
-        exit(EXIT_FAILURE); \
-        }
+	if ((x) == -1) { \
+		perror(msg); \
+		exit(EXIT_FAILURE); \
+	}
 
 #endif
--- main.c
+++ main.c
@@ -4,103 +4,223 @@
 int shmid = -1;
 int semid = -1;
 int msgid = -1;
+pid_t main_pid;
 StanSklepu *stan_sklepu = NULL;
 
 // Funkcja logowania (zapis do pliku z semaforem)
 void loguj(const char *msg) {
-        struct sembuf operacje[1];
-        operacje[0].sem_num = SEM_LOG;
-        operacje[0].sem_op = -1; // Czekaj (P)
-        operacje[0].sem_flg = 0;
+	struct sembuf operacje[1];
+	operacje[0].sem_num = SEM_LOG;
+	operacje[0].sem_op = -1; // Czekaj (P)
+	operacje[0].sem_flg = 0;
 
-        if (semop(semid, operacje, 1) == -1 && errno != EINTR) perror("Log sem wait error");
+	if (semop(semid, operacje, 1) == -1 && errno != EINTR) perror("Log sem wait error");
 
-        FILE *f = fopen("raport.txt", "a");
-        if (f) {
-                time_t now = time(NULL);
-                char *t = ctime(&now);
-                t[strlen(t)-1] = '\0'; // usun \n
-                fprintf(f, "[%s] %s\n", t, msg);
-                fclose(f);
-        }
+	FILE *f = fopen("raport.txt", "a");
+	if (f) {
+		time_t now = time(NULL);
+		char *t = ctime(&now);
+		t[strlen(t)-1] = '\0'; // usun \n
+		fprintf(f, "[%s] %s\n", t, msg);
+		fclose(f);
+	}
 
-        operacje[0].sem_op = 1; // Zwolnij (V)
-        if (semop(semid, operacje, 1) == -1 && errno != EINTR) perror("Log sem post error");
+	operacje[0].sem_op = 1; // Zwolnij (V)
+	if (semop(semid, operacje, 1) == -1 && errno != EINTR) perror("Log sem post error");
 }
 
 // Funkcja czyszczaca - wywolywana na koniec
 void sprzataj() {
-        printf("Sprzatanie zasobow IPC...\n");
-        if (shmid != -1) {
-                shmdt(stan_sklepu);
-                shmctl(shmid, IPC_RMID, NULL);
-        }
-        if (semid != -1) {
-                semctl(semid, 0, IPC_RMID);
-        }
-        if (msgid != -1) {
-                msgctl(msgid, IPC_RMID, NULL);
-        }
+	if (getpid() == main_pid) {
+		printf("Sprzatanie zasobow IPC...\n");
+		if (shmid != -1) {
+			shmdt(stan_sklepu);
+			shmctl(shmid, IPC_RMID, NULL);
+		}
+		if (semid != -1) {
+			semctl(semid, 0, IPC_RMID);
+		}
+		if (msgid != -1) {
+			msgctl(msgid, IPC_RMID, NULL);
+		}
+	}
 }
 
 // Obsluga sygnalu Ctrl+C, zeby posprzatac jak przerwiemy recznie
 void handle_sigint(int sig) {
-        exit(0); // To wywola funkcje zarejestrowana w atexit(sprzataj)
+	exit(0); // To wywola funkcje zarejestrowana w atexit(sprzataj)
+}
+
+// --- LOGIKA KLIENTA ---
+
+void proces_klient() {
+	pid_t my_pid = getpid();
+	srand(time(NULL) ^ my_pid); // Unikalne ziarno losowosci
+
+	// 1. Wejscie do sklepu (Aktualizacja licznika w Pamieci Dzielonej)
+	struct sembuf operacje[1];
+	operacje[0].sem_num = SEM_STAN;
+	operacje[0].sem_op = -1; // P (zablokuj dostep do stanu)
+	operacje[0].sem_flg = 0;
+	CHECK(semop(semid, operacje, 1), "semop P stan");
+
+	stan_sklepu->liczba_klientow_w_sklepie++;
+	int nr_klienta = stan_sklepu->liczba_klientow_w_sklepie;
+
+	operacje[0].sem_op = 1; // V (odblokuj)
+	CHECK(semop(semid, operacje, 1), "semop V stan");
+
+	char msg_buf[100];
+	sprintf(msg_buf, "Klient %d (PID: %d) wchodzi. Zakupy...", nr_klienta, my_pid);
+	loguj(msg_buf);
+
+	// 2. Symulacja zakupow (Losowy czas 1-5s)
+	int czas_zakupow = (rand() % 5) + 1;
+	sleep(czas_zakupow);
+
+	// 3. Wybor kasy i przygotowanie komunikatu
+	Komunikat kom;
+	kom.id_klienta = my_pid;
+	kom.liczba_produktow = (rand() % (MAX_PRODUKTOW - MIN_PRODUKTOW + 1)) + MIN_PRODUKTOW;
+	kom.czy_alkohol = (rand() % 100) < 20 ? 1 : 0; // 20% szans na alkohol
+
+	// Decyzja: 95% samoobslugowa, 5% stacjonarna
+	int los = rand() % 100;
+	if (los < 95) {
+		kom.mtype = 1; // 1 = Kasa Samoobslugowa
+		sprintf(msg_buf, "Klient %d idzie do samoobslugowej (Prod: %d, Alk: %d)", my_pid, kom.liczba_produktow, kom.czy_alkohol);
+	} else {
+		kom.mtype = 2; // 2 = Kasa Stacjonarna
+		sprintf(msg_buf, "Klient %d idzie do stacjonarnej (Prod: %d)", my_pid, kom.liczba_produktow);
+	}
+	loguj(msg_buf);
+
+	// 4. Ustawienie sie w kolejce (Wyslanie komunikatu)
+	if (msgsnd(msgid, &kom, sizeof(Komunikat) - sizeof(long), 0) == -1) {
+		perror("msgsnd klient");
+		exit(1);
+	}
+
+	// 5. Oczekiwanie na obsluge (Odbior komunikatu zwrotnego na kanal PID)
+	// TU KLIENT ZABLOKUJE SIE DO CZASU AZ KASJER GO OBSLUZY
+	Komunikat odpowiedz;
+	if (msgrcv(msgid, &odpowiedz, sizeof(Komunikat) - sizeof(long), my_pid, 0) == -1) {
+		perror("msgrcv klient");
+		exit(1);
+	}
+
+	sprintf(msg_buf, "Klient %d obsluzony. Wychodze.", my_pid);
+	loguj(msg_buf);
+
+	// 6. Wyjscie ze sklepu (Dekrementacja licznika)
+	operacje[0].sem_op = -1; // P
+	CHECK(semop(semid, operacje, 1), "semop P stan wyjscie");
+
+	stan_sklepu->liczba_klientow_w_sklepie--;
+
+	operacje[0].sem_op = 1; // V
+	CHECK(semop(semid, operacje, 1), "semop V stan wyjscie");
+
+	exit(0);
+}
+
+// --- GENERATOR KLIENTOW ---
+
+void generator_klientow() {
+	printf("Generator klientow uruchomiony (PID: %d)\n", getpid());
+	while (1) {
+		int current_count;
+		int koniec;
+		struct sembuf operacje[1];
+
+		// Pobranie licznika (sekcja krytyczna)
+		operacje[0].sem_num = SEM_STAN;
+		operacje[0].sem_op = -1;
+		operacje[0].sem_flg = 0;
+		semop(semid, operacje, 1);
+
+		current_count = stan_sklepu->liczba_klientow_w_sklepie;
+		koniec = stan_sklepu->koniec_symulacji;
+
+		operacje[0].sem_op = 1;
+		semop(semid, operacje, 1);
+
+		if (koniec) break;
+
+		if (current_count < MAX_KLIENTOW_W_SKLEPIE) {
+			pid_t pid = fork();
+			if (pid == 0) {
+				proces_klient(); // Proces potomny staje sie Klientem
+			} else if (pid == -1) {
+				perror("fork klient");
+			}
+		}
+
+		// Losowy odstep miedzy klientami
+		usleep(500000 + (rand() % 1500000));
+	}
+	exit(0);
 }
 
 int main() {
-        // 1. Rejestracja sprzatania
-        atexit(sprzataj);
-        signal(SIGINT, handle_sigint);
+	main_pid = getpid();
+	// 1. Rejestracja sprzatania
+	atexit(sprzataj);
+	signal(SIGINT, handle_sigint);
 
-        // 2. Tworzenie klucza
-        key_t key = ftok(FTOK_PATH, ID_PROJEKTU);
-        CHECK(key, "ftok");
+	// 2. Tworzenie klucza
+	key_t key = ftok(FTOK_PATH, ID_PROJEKTU);
+	CHECK(key, "ftok");
 
-        // 3. Tworzenie Pamieci Dzielonej
-        shmid = shmget(key, sizeof(StanSklepu), IPC_CREAT | 0600);
-        CHECK(shmid, "shmget");
+	// 3. Tworzenie Pamieci Dzielonej
+	shmid = shmget(key, sizeof(StanSklepu), IPC_CREAT | 0600);
+	CHECK(shmid, "shmget");
 
-        stan_sklepu = (StanSklepu*) shmat(shmid, NULL, 0);
-        if (stan_sklepu == (void*)-1) { perror("shmat"); exit(1); }
+	stan_sklepu = (StanSklepu*) shmat(shmid, NULL, 0);
+	if (stan_sklepu == (void*)-1) { perror("shmat"); exit(1); }
 
-        // Inicjalizacja pamieci (tylko raz, na poczatku!)
-        stan_sklepu->liczba_klientow_w_sklepie = 0;
-        stan_sklepu->kolejka_samoobslugowa_len = 0;
-        stan_sklepu->koniec_symulacji = 0;
-        // Domyslnie kasy stacjonarne zamkniete
-        stan_sklepu->kasy_stacjonarne_status[0] = 0;
-        stan_sklepu->kasy_stacjonarne_status[1] = 0;
-        // Domyslnie samoobslugowe wolne
-        for(int i=0; i<LICZBA_KAS_SAMOOBSLUGOWYCH; i++)
-                stan_sklepu->kasy_samoobslugowe_status[i] = 0;
+	// Inicjalizacja pamieci
+	stan_sklepu->liczba_klientow_w_sklepie = 0;
+	stan_sklepu->kolejka_samoobslugowa_len = 0;
+	stan_sklepu->koniec_symulacji = 0;
+	// Domyslnie kasy stacjonarne zamkniete
+	stan_sklepu->kasy_stacjonarne_status[0] = 0;
+	stan_sklepu->kasy_stacjonarne_status[1] = 0;
+	// Domyslnie samoobslugowe wolne
+	for(int i=0; i<LICZBA_KAS_SAMOOBSLUGOWYCH; i++)
+		stan_sklepu->kasy_samoobslugowe_status[i] = 0;
 
-        // 4. Tworzenie Semaforow
-        semid = semget(key, LICZBA_SEMAFOROW, IPC_CREAT | 0600);
-        CHECK(semid, "semget");
+	// 4. Tworzenie Semaforow
+	semid = semget(key, LICZBA_SEMAFOROW, IPC_CREAT | 0600);
+	CHECK(semid, "semget");
 
-        // Ustawienie wartosci poczatkowych semaforow na 1 (otwarte)
-        for (int i = 0; i < LICZBA_SEMAFOROW; i++) {
-                CHECK(semctl(semid, i, SETVAL, 1), "semctl SETVAL");
-        }
+	// Ustawienie wartosci poczatkowych semaforow na 1 (otwarte)
+	for (int i = 0; i < LICZBA_SEMAFOROW; i++) {
+		CHECK(semctl(semid, i, SETVAL, 1), "semctl SETVAL");
+	}
 
-        // 5. Tworzenie Kolejki Komunikatow
-        msgid = msgget(key, IPC_CREAT | 0600);
-        CHECK(msgid, "msgget");
+	// 5. Tworzenie Kolejki Komunikatow
+	msgid = msgget(key, IPC_CREAT | 0600);
+	CHECK(msgid, "msgget");
 
-        // Wyczysc stary plik raportu
-        FILE *f = fopen("raport.txt", "w");
-        if(f) { fprintf(f, "START SYMULACJI\n"); fclose(f); }
+	// Wyczysc stary plik raportu
+	FILE *f = fopen("raport.txt", "w");
+	if(f) { fprintf(f, "START SYMULACJI\n"); fclose(f); }
 
-        printf("IPC zainicjalizowane. Start procesow...\n");
+	printf("IPC zainicjalizowane. Start procesow...\n");
 
-        // TU BEDA TWORZONE PROCESY (FORK)
+	// 6. Uruchomienie Generatora Klientow
+	pid_t pid_gen = fork();
+	if (pid_gen == 0) {
+		generator_klientow();
+	}
 
-        // Na razie symuluje oczekiwanie, zeby sprawdzic czy IPC dziala
+	// Tutaj dodac Kierownika i Kasjerow
 
-        // tu bedzie wait() na procesy potomne
-        printf("Nacisnij Ctrl+C aby zakonczyc i posprzatac...\n");
-        while(1) sleep(1);
+	printf("Symulacja dziala. Nacisnij Ctrl+C aby zakonczyc...\n");
 
-        return 0;
+	// Oczekiwanie na zakonczenie procesow potomnych (zeby nie zrobic zombie)
+	while(wait(NULL) > 0);
+
+	return 0;
 }
